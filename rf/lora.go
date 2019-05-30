3de3748cb35d55c3d87aacb203f9a6c03a6b6c2c
weixingsun <weixing.sun@gmail.com>
2019-05-30 20:36:34 +0800

    first commit, working on inav/cleanflight

---
+++ rf/lora.go
@@ -0,0 +1,80 @@
+package main
+import (
+    "fmt"
+    "io"
+    "log"
+    "github.com/jacobsa/go-serial/serial"
+    //"flag"
+    //"os"
+    //"encoding/hex"
+)
+type fn func(string)
+//type RF interface {
+//    send(string)
+//    listen(fn)
+//}
+type Lora struct {
+    //send() void
+    //listen() void
+    dev, encoder, addr string
+    baudrate uint
+    //oo       serial.OpenOptions
+    port     io.ReadWriteCloser
+}
+
+func NewLora(dev string,baudrate uint,addr string,encoder string)*Lora{
+    lora := new(Lora)
+    lora.dev = dev
+    lora.baudrate = baudrate
+    lora.addr = addr
+    lora.encoder = encoder
+    oo := serial.OpenOptions{
+        PortName: dev,
+        BaudRate: baudrate,
+        DataBits: 8,
+        StopBits: 1,
+        MinimumReadSize: 4,
+    }
+    port, err := serial.Open(oo)
+    if err != nil {
+        log.Fatalf("Error Serial.Open: %v", err)
+    }
+    lora.port = port
+    return lora
+}
+
+func (l *Lora) send(msg string) {
+    //txData_, err := hex.DecodeString(msg)
+    b := []byte(msg)
+    l.port.Write(b)
+}
+
+func (l *Lora) listen(f fn) {
+    for {
+        buf := make([]byte, 32)
+        n, err := l.port.Read(buf)
+        if err != nil {
+            if err != io.EOF {
+                fmt.Println("Error reading from serial port: ", err)
+            }
+        } else {
+            buf = buf[:n]
+            f(string(buf))
+        }
+    }
+}
+
+func (l Lora) sprint(msg string) {
+    fmt.Println(msg)
+}
+func main() {
+    l := NewLora(
+        "/dev/UART_CP2102",
+        9600,
+        "utf-8",
+        "FFFF",
+    )
+    //defer lora.port.Close()  //when to close ?
+    l.listen(l.sprint)
+    //l.send("haha")
+}
---
+++ test.sh
@@ -0,0 +1,1 @@
+bin/takeoff -d /dev/UART_CF -a
