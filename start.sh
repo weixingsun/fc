4f1b557c3637012192ab606cad490939a353b290
weixingsun <weixing.sun@gmail.com>
2019-06-05 15:54:24 +0800

    cross compile

--- start.sh
+++ start.sh
@@ -1,1 +1,14 @@
-nohup /root/fc_go/fc_go > /root/fc_go/log/start.log 2>&1 &
+#!/bin/bash
+arch=""
+case $(uname -m) in
+  i386)   arch="386" ;;
+  i686)   arch="386" ;;
+  x86_64) arch="amd64" ;;
+  arm)    dpkg --print-architecture | grep -q "arm64" && arch="arm64" || arch="arm" ;;
+esac
+#echo $arch
+
+SCRIPT=$(readlink -f $0)
+DIR=`dirname $SCRIPT`
+mkdir -p $DIR/log
+nohup $DIR/bin/fc_$arch > $DIR/log/start.log 2>&1 &
